05601898b98ffc2b02ac36bf3a5d33d1f6c7cded
e-n-p <nickellam0@gmail.com>
2020-07-06 20:04:07 +0100

    removing old unused code

--- src/h/img/rec/ImageComparison.java
+++ src/h/img/rec/ImageComparison.java
@@ -30,12 +30,12 @@ class ImageComparison {
     }
 
     void setStoredScore(int storedScore) { this.storedScore = storedScore; }
+    int getStoredScore() { return storedScore; }
 
     int getH() { return h; }
     int getW() { return w; }
-    int getStoredScore() { return storedScore; }
-    BufferedImage getTargetImg(){ return targetImg; }
 
+    BufferedImage getTargetImg(){ return targetImg; }
     void setTargetImage(BufferedImage TargetImg) {
         this.targetImg = TargetImg;
         this.h = targetImg.getHeight();
@@ -75,34 +75,3 @@ class ImageComparison {
     }
 
 }
-//    double comparisonRect(BufferedImage inputImage, int[] pos) {
-//        int highW, lowW, highH, lowH;
-//        if (pos[0] < pos[2]) {
-//            highW = pos[2];
-//            lowW = pos[0];
-//        } else {
-//            highW = pos[0];
-//            lowW = pos[2];
-//        }
-//        if (pos[1] < pos[3]) {
-//            highH = pos[3];
-//            lowH = pos[1];
-//        } else {
-//            highH = pos[1];
-//            lowH = pos[3];
-//        }
-//        long diff = 0;
-//        //System.out.println("lowH: "+ lowH+" highH: "+highH);
-//        //System.out.println("lowW: "+ lowW+" highW: "+highW);
-//        for (int i = lowH; i <= highH; i++) {
-//            for (int j = lowW; j <= highW; j++) {
-//                Color aColor = new Color(inputImage.getRGB(j, i));
-//                Color bColor = new Color(this.targetImg.getRGB(j, i));
-//                int redDiff = Math.abs(aColor.getRed() - bColor.getRed());
-//                int blueDiff = Math.abs(aColor.getBlue() - bColor.getBlue());
-//                int greenDiff = Math.abs(aColor.getGreen() - bColor.getGreen());
-//                diff += redDiff + blueDiff + greenDiff;
-//            }
-//        }
-//        return diff;
-//    }
